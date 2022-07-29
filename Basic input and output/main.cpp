d953c233be8c780173a249f50905ad9e9231f09d
Muaz Bin Gulfam <106175817+Dangeryego@users.noreply.github.com>
2022-07-30 01:38:48 +0500

    For code: Open main.cpp

---
+++ Basic input and output/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    float num2;
+    double num3;
+
+//    cout << "Enter 3 numbers" << endl;
+//    cin >> num1 >> num2 >> num3;
+//    cout << "Integer is: " << num1 << "\n"<< "Float is: " << num2 << "\n" << "Double is: " << num3;
+
+    cout << "Enter an Integer/n";
+    cin >> num1;
+    cout << "Enter a Float\n";
+    cin >> num2;
+    cout << "Enter a double\n";
+    cin >> num3;
+    cout << num1 << num2 << num3;
+
+
+
+    return 0;
+}
